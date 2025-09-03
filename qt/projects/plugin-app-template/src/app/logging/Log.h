825ff220a2cf7afd3161d6cbc003853fa2551ff9
Joachim Coline <joachim.coline@gmail.com>
2025-09-03 19:56:34 +0200

    ADD: use source file parent dir as logging category.

--- qt/projects/plugin-app-template/src/app/logging/Log.h
+++ qt/projects/plugin-app-template/src/app/logging/Log.h
@@ -3,21 +3,84 @@
 #include <QLoggingCategory>
 #include <QDebug>
 
-constexpr const char* categoryNameFromPath(const char* path) {
-    const char* file = path;
-    for (const char* p = path; *p; ++p) {
-        if (*p == '/' || *p == '\\') {
-            file = p + 1;
+/* Macros for logging.
+ * Logging categories are generated automatically and at compile time.
+ */
+
+namespace JApp {
+    namespace Internal {
+        constexpr size_t findLastSeparator(const char* path) {
+            size_t i = 0;
+            size_t lastSep = 0;
+
+            while (path[i]) {
+                if (path[i] == '/' || path[i] == '\\') {
+                    lastSep = i;
+                }
+                ++i;
+            }
+
+            return lastSep;
+        }
+
+        constexpr size_t findPenultimateSeparator(const char* path, size_t lastSep) {
+            size_t penultimateSep = 0;
+
+            for (size_t i = 0; i < lastSep; ++i) {
+                if (path[i] == '/' || path[i] == '\\') {
+                    penultimateSep = i;
+                }
+            }
+
+            return penultimateSep;
+        }
+
+        template <size_t N>
+        struct StringLiteral {
+            char data[N];
+            constexpr StringLiteral() : data{} {}
+            constexpr operator const char*() const {
+                return data;
+            }
+        };
+
+        template <size_t MaxSize = 64>
+        constexpr auto categoryNameFromPath(const char* path) {
+            StringLiteral<MaxSize> result = {};
+
+            const size_t lastSep = findLastSeparator(path);
+            if (lastSep == 0) {
+                result.data[0] = '.';
+                result.data[1] = '\0';
+                return result;
+            }
+
+            const size_t penultimateSep = findPenultimateSeparator(path, lastSep);
+            const size_t start = penultimateSep + 1;
+            const size_t length = lastSep - start;
+
+            // Copy the parent directory name
+            for (size_t i = 0; i < length && i < MaxSize - 1; ++i) {
+                result.data[i] = path[start + i];
+            }
+            result.data[length < MaxSize - 1 ? length : MaxSize - 1] = '\0';
+
+            return result;
         }
     }
-    return file;
 }
+
+#define CATEGORY_NAME_FROM_PATH() \
+    []() { \
+            static constexpr auto name = JApp::Internal::categoryNameFromPath(__FILE__); \
+            static constexpr const char* str = name; \
+            return str; \
+    }()
 
 #define CURRENT_LOG_CATEGORY() \
     []() -> const QLoggingCategory& { \
-        constexpr auto categoryName = categoryNameFromPath(__FILE__); \
-        static const QLoggingCategory category(categoryName); \
-        return category; \
+            static const QLoggingCategory category(CATEGORY_NAME_FROM_PATH()); \
+            return category; \
     }()
 
 #define LOG_DEBUG()    qCDebug(CURRENT_LOG_CATEGORY())
