3e2bf3285b88a51086764ceae2459fe9bc23f301
Joachim Coline <joachim.coline@gmail.com>
2025-09-03 20:46:06 +0200

    CHG: move auto logging category details to separate header

--- qt/projects/plugin-app-template/src/app/logging/include/JApp/Log.h
+++ qt/projects/plugin-app-template/src/app/logging/include/JApp/Log.h
@@ -1,78 +1,12 @@
 #pragma once
 
+#include "JApp/Internal/LogUtils.h"
 #include <QLoggingCategory>
 #include <QDebug>
 
-/* Macros for logging.
- * Logging categories are generated automatically and at compile time.
- */
-
-namespace JApp {
-    namespace Internal {
-        constexpr size_t findLastSeparator(const char* path) {
-            size_t i = 0;
-            size_t lastSep = 0;
-
-            while (path[i]) {
-                if (path[i] == '/' || path[i] == '\\') {
-                    lastSep = i;
-                }
-                ++i;
-            }
-
-            return lastSep;
-        }
-
-        constexpr size_t findPenultimateSeparator(const char* path, size_t lastSep) {
-            size_t penultimateSep = 0;
-
-            for (size_t i = 0; i < lastSep; ++i) {
-                if (path[i] == '/' || path[i] == '\\') {
-                    penultimateSep = i;
-                }
-            }
-
-            return penultimateSep;
-        }
-
-        template <size_t N>
-        struct StringLiteral {
-            char data[N];
-            constexpr StringLiteral() : data{} {}
-            constexpr operator const char*() const {
-                return data;
-            }
-        };
-
-        template <size_t MaxSize = 64>
-        constexpr auto categoryNameFromPath(const char* path) {
-            StringLiteral<MaxSize> result = {};
-
-            const size_t lastSep = findLastSeparator(path);
-            if (lastSep == 0) {
-                result.data[0] = '.';
-                result.data[1] = '\0';
-                return result;
-            }
-
-            const size_t penultimateSep = findPenultimateSeparator(path, lastSep);
-            const size_t start = penultimateSep + 1;
-            const size_t length = lastSep - start;
-
-            // Copy the parent directory name
-            for (size_t i = 0; i < length && i < MaxSize - 1; ++i) {
-                result.data[i] = path[start + i];
-            }
-            result.data[length < MaxSize - 1 ? length : MaxSize - 1] = '\0';
-
-            return result;
-        }
-    }
-}
-
 #define CATEGORY_NAME_FROM_PATH() \
     []() { \
-            static constexpr auto name = JApp::Internal::categoryNameFromPath(__FILE__); \
+            static constexpr auto name = JApp::Internal::LogUtils::categoryNameFromPath(__FILE__); \
             static constexpr const char* str = name; \
             return str; \
     }()
