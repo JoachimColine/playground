995d2203d04f1d88b6105489775b8abb8e3dbae4
Joachim Coline <joachim.coline@gmail.com>
2025-09-04 20:23:47 +0200

    BUG: don't use 'src' as logging category.

--- qt/projects/plugin-app-template/src/app/logging/internal/JApp/Internal/LogUtils.h
+++ qt/projects/plugin-app-template/src/app/logging/internal/JApp/Internal/LogUtils.h
@@ -30,6 +30,27 @@ namespace JApp {
                     return penultimateSep;
                 }
 
+                constexpr size_t findAntepenultimateSeparator(const char* path, size_t penultimateSep) {
+                    size_t antepenultimateSep = 0;
+
+                    for (size_t i = 0; i < penultimateSep; ++i) {
+                        if (path[i] == '/' || path[i] == '\\') {
+                            antepenultimateSep = i;
+                        }
+                    }
+
+                    return antepenultimateSep;
+                }
+
+                constexpr bool strEquals(const char* str1, const char* str2, size_t len) {
+                    for (size_t i = 0; i < len; ++i) {
+                        if (str1[i] != str2[i] || str1[i] == '\0' || str2[i] == '\0') {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+
                 template <size_t N>
                 struct StringLiteral {
                     char data[N];
@@ -55,11 +76,24 @@ namespace JApp {
 				const size_t start = penultimateSep + 1;
 				const size_t length = lastSep - start;
 
-				// Copy the parent directory name
-				for (size_t i = 0; i < length && i < MaxSize - 1; ++i) {
-					result.data[i] = path[start + i];
-				}
-				result.data[length < MaxSize - 1 ? length : MaxSize - 1] = '\0';
+                const bool isSrc = length == 3 &&
+                                   detail::strEquals(path + start, "src", 3);
+
+                if (isSrc) {
+                    const size_t antepenultimateSep = detail::findAntepenultimateSeparator(path, penultimateSep);
+                    const size_t grandparentStart = antepenultimateSep + 1;
+                    const size_t grandparentLength = penultimateSep - grandparentStart;
+
+                    for (size_t i = 0; i < grandparentLength && i < MaxSize - 1; ++i) {
+                        result.data[i] = path[grandparentStart + i];
+                    }
+                    result.data[grandparentLength < MaxSize - 1 ? grandparentLength : MaxSize - 1] = '\0';
+                } else {
+                    for (size_t i = 0; i < length && i < MaxSize - 1; ++i) {
+                        result.data[i] = path[start + i];
+                    }
+                    result.data[length < MaxSize - 1 ? length : MaxSize - 1] = '\0';
+                }
 
 				return result;
 			}
